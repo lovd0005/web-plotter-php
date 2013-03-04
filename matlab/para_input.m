cbc6f6e6acc940c17b6e4884eb41218be0706078
cwu@spa <cwu@physics.umn.edu>
2013-03-04 03:37:01 -0600

    add gitignore;

--- matlab/para_input.m
+++ matlab/para_input.m
@@ -2,16 +2,15 @@ xmin = 1;
 
 xmax = 1000;
 
-ymin = 1.0E-18;
+ymin = 1E-18;
 
-ymax = 1.0E-16;
+ymax = 1E-16;
 
 numpoints = 500;
 
-mod_number = 2;
+mod_number = 1;
 
 1E+15
-1.22
 sfrate = 'h';
 
 manually = 0;
